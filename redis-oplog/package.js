8b688157ffd30fda79f51342f507b437fffa7f6c
matheusccastro <camargodecastromatheus@gmail.com>
2024-05-08 00:02:44 -0300

    redis-oplog@3.0.0 - update git url

--- redis-oplog/package.js
+++ redis-oplog/package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: "Replacement for Meteor's MongoDB oplog implementation",
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/cult-of-coders/redis-oplog',
+  git: 'https://github.com/quavedev/meteor-packages',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md',
