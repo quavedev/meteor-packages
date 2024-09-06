18188b00e10b5b7f741f1513f113e92104b80a6a
matheusccastro <camargodecastromatheus@gmail.com>
2024-09-06 19:41:46 -0300

    quave:quave:collection-hooks@2.0.0 - don't monkey patch `find` method

--- meteor-collection-hooks/find.js
+++ meteor-collection-hooks/find.js
@@ -1,7 +1,5 @@
 import { CollectionHooks } from './collection-hooks';
 
-const ASYNC_METHODS = ['countAsync', 'fetchAsync', 'forEachAsync', 'mapAsync'];
-
 /**
  * With Meteor v3 this behaves differently than with Meteor v2.
  * We cannot use async hooks on find() directly because in Meteor it is a sync method that returns cursor instance.
@@ -20,75 +18,11 @@ CollectionHooks.defineAdvice(
     args,
     suppressAspects
   ) {
-    // const ctx = { context: this, _super, args }
     const selector = CollectionHooks.normalizeSelector(
       instance._getFindSelector(args)
     );
     const options = instance._getFindOptions(args);
 
-    const cursor = _super.call(this, selector, options);
-
-    // Wrap async cursor methods
-    ASYNC_METHODS.forEach((method) => {
-      if (cursor[method]) {
-        cursor[method] = async (...args) => {
-          let abort = false;
-          for (const aspect of aspects.before) {
-            const result = await aspect.aspect.call(
-              this,
-              userId,
-              selector,
-              options
-            );
-            if (result === false) {
-              abort = true;
-            }
-          }
-
-          // Take #1 - monkey patch existing cursor
-          // Now that before hooks have run, update the cursor selector & options
-          // Special case for "undefined" selector, which means none of the documents
-          // This is a full c/p from Meteor's  minimongo/cursor.js, it probably doesn't make too much sense and is too
-          // error-prone to maintain as each Meteor change would require
-
-          // cursor.sorter = null
-          // cursor.matcher = new Minimongo.Matcher(Mongo.Collection._rewriteSelector(abort ? undefined : selector))
-          // if (Minimongo.LocalCollection._selectorIsIdPerhapsAsObject(selector)) {
-          //   // eslint-disable-next-line no-prototype-builtins
-          //   cursor._selectorId = Object.prototype.hasOwnProperty(selector, '_id') ? selector._id : selector
-          // } else {
-          //   cursor._selectorId = undefined
-          //   if (cursor.matcher.hasGeoQuery() || options.sort) {
-          //     cursor.sorter = new Minimongo.Sorter(options.sort || [])
-          //   }
-          // }
-          // cursor.skip = options.skip || 0
-          // cursor.limit = options.limit
-          // cursor.fields = options.projection || options.fields
-          // cursor._projectionFn = Minimongo.LocalCollection._compileProjection(cursor.fields || {})
-          // cursor._transform = Minimongo.LocalCollection.wrapTransform(options.transform)
-          // if (typeof Tracker !== 'undefined') {
-          //   cursor.reactive = options.reactive === undefined ? true : options.reactive
-          // }
-
-          // Take #2 - create new cursor
-          const newCursor = _super.call(
-            this,
-            abort ? undefined : selector,
-            options
-          );
-
-          const result = await newCursor[method](...args);
-
-          for (const aspect of aspects.after) {
-            await aspect.aspect.call(this, userId, selector, options, cursor);
-          }
-
-          return result;
-        };
-      }
-    });
-
-    return cursor;
+    return _super.call(this, selector, options);
   }
 );
