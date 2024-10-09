6dc9b5665dbdd8b6b7f785889a44581a02fac2f0
Gabriel Araújo <gabrielrudy575@gmail.com>
2024-10-09 15:05:21 -0300

    Slingshot - Cloudflare R2 compatibility
    bump up version

--- meteor-slingshot/package.js
+++ meteor-slingshot/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'quave:slingshot',
   summary: 'Directly post files to cloud storage services, such as AWS-S3.',
-  version: '2.0.1',
+  version: '2.0.2',
   git: 'https://github.com/quavedev/meteor-packages',
 });
 
